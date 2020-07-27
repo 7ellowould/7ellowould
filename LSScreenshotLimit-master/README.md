cacd779f9f805e6bb9687ec3c919cbf19c5e8b61
7ellowould <7ellowould@gmail.com>
2020-07-28 00:01:10 +0300

    Update

--- LSScreenshotLimit-master/README.md
+++
@@ -1,2 +0,0 @@
-# LSScreenshotLimit
-Set a limit to how many screenshots can be taken when on lockscreen
--- LSScreenshotLimit-master/lsscreenshotlimit/LSLRootListController.h
+++
@@ -1,6 +0,0 @@
-#import <Preferences/PSListController.h>
-#import <MessageUI/MFMailComposeViewController.h>
-
-@interface LSLRootListController : PSListController <MFMailComposeViewControllerDelegate>
-
-@end
